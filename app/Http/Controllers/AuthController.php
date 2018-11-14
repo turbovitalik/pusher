7bc176c6edc243b576ff184cdc6a3c7bbf98be31
Vitalii Onanko <v.onanko@8ways.ch>
2018-11-14 17:47:57 +0800

    JWT logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -13,7 +14,14 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('JWT');
+//        $this->middleware('auth:api', ['except' => ['login', 'signup']]);
+    }
+
+    public function signup(Request $request)
+    {
+        User::create($request->all());
+        return $this->login($request);
     }
 
     /**
